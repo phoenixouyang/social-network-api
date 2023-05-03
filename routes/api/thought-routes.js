11e56a838cf05ecb9fed4281caa5be8ed2ed8fd4
Pholobo <ouyangphoenix@gmail.com>
2023-05-03 16:59:00 -0400

    adds api routes for users and thoughts

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+router
+    .route('/thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
