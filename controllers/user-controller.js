b8eed3a85204a6f424fc7e246db2f66b9b1f7f03
Pholobo <ouyangphoenix@gmail.com>
2023-05-03 16:25:34 -0400

    adds controllers to create, update and delete users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,20 +15,72 @@ module.exports = {
   // get a singer user by _id
   async getSingleUser(req, res) {
     try {
-      const user = await Users.findOne({ _id: req.params.id })
+      const user = await Users.findOne({ _id: req.params.userId })
         .populate({
-            path: 'thoughts',
-            select: '-__v',
+          path: 'thoughts',
+          select: '-__v',
+        })
+        .populate({
+          path: 'friends',
+          select: '-__v',
         })
         if (!user) {
           return res.status(404).json({ message: 'Sorry, no user was found with that ID' })
         }
-        return res.json({student});
+        res.json(user);
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
     }
-  }
+  },
 
-  
+  // create user
+  async createUser(req, res) {
+    try {
+      const user = await Users.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // update a single user by _id
+  async updateUser(req, res) {
+    try {
+      const user = await Users.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true },
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'Sorry, no user was found with that ID' })
+      }
+      res.json(user)
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // delete a single user by _id, and all associated thoughts
+  async deleteUser(req, res) {
+    try {
+      const user = await Users.findOneAndRemove({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'Sorry, no user was found with that ID' })
+      }
+
+      Thoughts.deleteMany({ userId: req.params.id })
+
+      res.json({ message: 'User has been deleted'});
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
 }
